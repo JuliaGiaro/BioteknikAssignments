7a55d5b59c4db2f85c2abe48715dce1a8ea780e9
julia <juliagiaro@hotmail.com>
2023-11-27 11:07:48 +0100

    got graph representation but clusters wont connect as expected

--- AlphaCarbonOrder.py
+++ AlphaCarbonOrder.py
@@ -1,86 +1,193 @@
-def read_file(file_name):
-    """
-    Read the file and store the data in a list.
-    Each element of the list is a tuple of the form (atom_serial_number, x_coordinate, y_coordinate, z_coordinate).
-    """
-    file_name = "data_q1.txt"
-    data_file = []
-    with open(file_name, 'r') as file:
-        for line in file:
-            split_line = line.split()
-            data_file.append({
-                'atom_serial_number': int(split_line[0]),
-                'x_coordinate': float(split_line[1]),
-                'y_coordinate': float(split_line[2]),
-                'z_coordinate': float(split_line[3])
-            })
-    return data_file
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+# Function to calculate distance between two points
+def calculate_distance(point1, point2):
+    return np.sqrt(np.sum((point1 - point2)**2))
+
+# Function to find the nearest neighbor for each point
+def find_nearest_neighbors(coordinates):
+    connections = []
+    n = len(coordinates)
+
+    for i in range(n):
+        min_distance = float('inf')
+        nearest_point = None
+
+        for j in range(n):
+            if i != j:
+                distance = calculate_distance(coordinates[i, 1:], coordinates[j, 1:])
+                if distance < min_distance:
+                    min_distance = distance
+                    nearest_point = j
+
+        if nearest_point is not None:
+            connections.append((int(coordinates[i, 0]), int(coordinates[nearest_point, 0])))
+
+    return connections
+
+# Function to perform depth-first search traversal
+def dfs(graph, start, visited, order):
+    visited[start] = True
+    order.append(start)
+
+    for neighbor in graph[start]:
+        if not visited[neighbor]:
+            dfs(graph, neighbor, visited, order)
+
+# Function to find and print the order of alpha carbons
+def print_order(connections, coordinates):
+    graph = {i: [] for i in range(1, 11)}
+
+    for connection in connections:
+        graph[connection[0]].append(connection[1])
 
 
-def read_test_file(file_name):
-    file_name = "test_q1.txt"
-    test_file = []
-    with open(file_name, 'r') as file:
-        for line in file:
-            split_line = line.split()
-            test_file.append({
-                'atom_serial_number': int(split_line[0]),
-                'x_coordinate': float(split_line[1]),
-                'y_coordinate': float(split_line[2]),
-                'z_coordinate': float(split_line[3])
-            })
-    return test_file
+    visited = {i: False for i in range(1, 11)}
+    order = []
+
+    # Find the alpha carbon with the lowest X and Y coordinates as the starting point
+    starting_point = find_starting_point(coordinates)
+    dfs(graph, starting_point, visited, order)
+
+    print(order)
+
+# Function to find the alpha carbon with the lowest X and Y coordinates
+def find_starting_point(coordinates):
+    min_xy_index = np.argmin(coordinates[:, 1] + coordinates[:, 2])
+    return int(coordinates[min_xy_index, 0])
+
+def connect_clusters(coordinates, threshold_distance):
+    connections = find_nearest_neighbors(coordinates)
+    clusters = []
+
+    for connection in connections:
+        connected = False
+
+        for cluster in clusters:
+            for point in cluster:
+                distance = calculate_distance(coordinates[connection[0] - 1, 1:], coordinates[point - 1, 1:])
+                if distance < threshold_distance:
+                    cluster.append(connection[1])
+                    connected = True
+                    break
+
+        if not connected:
+            clusters.append([connection[0], connection[1]])
+
+    # Connect clusters into one directed graph
+    directed_connections = []
+
+    for i in range(len(clusters) - 1):
+        current_cluster = clusters[i]
+        next_cluster = clusters[i + 1]
+
+        # Find nearest endpoint in the next cluster from the current cluster's endpoint
+        current_endpoint = current_cluster[-1]
+        min_distance = float('inf')
+        nearest_point = None
+
+        for point in next_cluster:
+            distance = calculate_distance(coordinates[current_endpoint - 1, 1:], coordinates[point - 1, 1:])
+            if distance < min_distance:
+                min_distance = distance
+                nearest_point = point
+
+        # Add directed connection
+        directed_connections.append((current_endpoint, nearest_point))
+
+    return directed_connections
 
 
-def calculate_distance(atom1, atom2):
-    """
-    Calculate the Euclidean distance between two atoms.
-    """
-    return ((atom2['x_coordinate'] - atom1['x_coordinate']) ** 2 +
-            (atom2['y_coordinate'] - atom1['y_coordinate']) ** 2 +
-            (atom2['z_coordinate'] - atom1['z_coordinate']) ** 2) ** 0.5
+# Function to plot 3D coordinates with color-coded points, annotations, and connections
+def plot_coordinates_with_connections(coordinates, title):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Extract x, y, z coordinates
+    x = coordinates[:, 1]
+    y = coordinates[:, 2]
+    z = coordinates[:, 3]
+
+    # Color-coded points based on numbers
+    colors = coordinates[:, 0]
+
+    # Scatter plot with color-coded points
+    scatter = ax.scatter(x, y, z, c=colors, cmap='viridis', marker='o', s=50, label='Alpha Carbons')
+
+    # Add a color bar
+    cbar = plt.colorbar(scatter)
+    cbar.set_label('Alpha Carbon Number')
+
+    # Annotate points with alpha carbon numbers
+    for i, txt in enumerate(coordinates[:, 0]):
+        ax.text(x[i], y[i], z[i], str(int(txt)), color='black')
+
+    # Find and plot connections between alpha carbons
+    connections = find_nearest_neighbors(coordinates)
+
+    # Sort connections based on the X-coordinate
 
 
-def check_alpha_carbon_distances(data_file, max_distance_difference=4.0, expected_distance=3.8):
-    """
-    Check if the distances between alpha-carbon atoms are approximately equal to the expected distance.
-    """
-    # Find all atoms in the data
-    all_atoms = data_file
-    unique_alpha_carbon_atoms = set()
+    # Print connected alpha carbons
 
 
+    for connection in connections:
+        ax.plot([x[connection[0] - 1], x[connection[1] - 1]],
+                [y[connection[0] - 1], y[connection[1] - 1]],
+                [z[connection[0] - 1], z[connection[1] - 1]], color='gray', linestyle='--')
+    threshold_distance = 4.0 # Adjust this threshold distance as needed
+    clusters = connect_clusters(coordinates, threshold_distance)
 
-    # Check for potential alpha-carbon atoms based on a more lenient proximity check
-    for atom in all_atoms:
-        if (abs(atom['x_coordinate'] - round(atom['x_coordinate'])) < max_distance_difference
-                and abs(atom['y_coordinate'] - round(atom['y_coordinate'])) < max_distance_difference
-                and abs(atom['z_coordinate'] - round(atom['z_coordinate'])) < max_distance_difference):
-            unique_alpha_carbon_atoms.add(atom['atom_serial_number'])
+    # Plot connections between clusters
+    for cluster in clusters:
+        for i in range(len(cluster) - 1):
+            ax.plot([x[cluster[i] - 1], x[cluster[i + 1] - 1]],
+                    [y[cluster[i] - 1], y[cluster[i + 1] - 1]],
+                    [z[cluster[i] - 1], z[cluster[i + 1] - 1]], color='gray', linestyle='--')
+    print_order(connections, coordinates)
+    # Label axes
+    ax.set_xlabel('X (Å)')
+    ax.set_ylabel('Y (Å)')
+    ax.set_zlabel('Z (Å)')
 
-    print(f"Number of potential alpha-carbon atoms: {len(unique_alpha_carbon_atoms)}")
+    # Set title
+    ax.set_title(title)
 
-    # Check distances between alpha-carbon atoms
-    alpha_carbon_order = list(unique_alpha_carbon_atoms)
-    alpha_carbon_order.sort(key=lambda x: (x, calculate_total_distance(x, all_atoms)))
+    # Show plot
+    plt.show()
 
-    # Print the order of alpha-carbon atoms based on distances
-    print(f"Order of alpha-carbon atoms based on distances: {alpha_carbon_order}")
+# Coordinates for the first file
+file1_coordinates = np.array([
+    [1, 5.598, 5.767, 11.082],
+    [2, 8.496, 4.609, 8.837],
+    [3, 13.660, 10.707, 9.787],
+    [4, 10.646, 8.991, 11.408],
+    [5, 8.912, 2.083, 13.258],
+    [6, 9.448, 9.034, 15.012],
+    [7, 8.673, 5.314, 15.279],
+    [8, 16.967, 12.784, 4.338],
+    [9, 13.856, 11.469, 6.066],
+    [10, 5.145, 2.209, 12.453]
+])
 
+# Coordinates for the second file
+file2_coordinates = np.array([
+    [1, 13.257, 10.745, 15.081],
+    [2, 13.512, 5.395, 12.878],
+    [3, 13.564, 11.573, 18.836],
+    [4, 15.445, 7.667, 15.246],
+    [5, 17.334, 10.956, 18.691],
+    [6, 17.924, 13.421, 15.877],
+    [7, 18.504, 12.312, 12.298],
+    [8, 21.452, 16.969, 6.513],
+    [9, 21.936, 12.911, 10.809],
+    [10, 22.019, 13.242, 7.020]
+])
 
-def calculate_total_distance(atom_serial_number, atom_list):
-    """Calculate the total distance between an atom and all other alpha-carbon atoms."""
-    atom = find_atom(atom_serial_number, atom_list)
-    distances = [calculate_distance(atom, other) for other in atom_list if other['atom_serial_number'] != atom_serial_number]
-    return sum(distances)
+# Plot coordinates for the first file with color-coded points, annotations, and connections
+plot_coordinates_with_connections(file1_coordinates, 'Alpha Carbons - File 1')
 
-def find_atom(atom_serial_number, atom_list):
-    """Helper function to find an atom in the list based on its serial number."""
-    for atom in atom_list:
-        if atom['atom_serial_number'] == atom_serial_number:
-            return atom
-    raise ValueError(f"Atom with serial number {atom_serial_number} not found in the provided list.")
-if __name__ == "__main__":
-    data_file = read_file("data_q1.txt")
-    check_alpha_carbon_distances(data_file)
-
+# Plot coordinates for the second file with color-coded points, annotations, and connections
+plot_coordinates_with_connections(file2_coordinates, 'Alpha Carbons - File 2')
